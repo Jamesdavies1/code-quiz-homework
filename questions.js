c69073b9d5e3f90f05a7e5664554321c50611c1b
james davies <james.davies92@icloud.com>
2020-01-13 20:25:36 +0000

    added timer in js to countdown from 2minutes when entering the quiz page. after timer hits 0 triggers alert saying times up

--- questions.js
+++ questions.js
@@ -1,219 +1,247 @@
-(function() {
+var mins = 2;
+var secs = mins * 60;
+
+function countdown() {
+    setTimeout('Decrement()', 60);
+}
+
+function Decrement() {
+    if (document.getElementById) {
+        minutes = document.getElementById("minutes");
+        seconds = document.getElementById("seconds");
+
+        if (seconds < 59) {
+            seconds.value = secs;
+        }
+
+        else {
+            minutes.value = getminutes();
+            seconds.value = getseconds();
+        }
+
+        if (mins < 1) {
+            minutes.style.color = "red";
+            seconds.style.color = "red";
+        }
+
+        if (mins < 0) {
+            alert('time up');
+            minutes.value = 0;
+            seconds.value = 0;
+        }
+        else {
+            secs--;
+            setTimeout('Decrement()', 1000);
+        }
+    }
+}
+
+function getminutes() {
+    mins = Math.floor(secs / 60);
+    return mins;
+}
+
+function getseconds() {
+    return secs - Math.round(mins * 60);
+}
+
+(function () {
     const myQuestions = [
-      {
-        question: "What is my dog called?",
-        answers: {
-          A: "Rio",
-          B: "Lassie",
-          C: "River",
-          D: "Misty",
+        {
+            question: "What is my dog called?",
+            answers: {
+                A: "Rio",
+                B: "Lassie",
+                C: "River",
+                D: "Misty",
+            },
+            correctAnswer: "C"
         },
-        correctAnswer: "C"
-      },
-      {
-        question: "Where do i live?",
-        answers: {
-          A: "Cheshire",
-          B: "Manchester",
-          C: "Stoke",
-          D: "Chester",
+        {
+            question: "Where do i live?",
+            answers: {
+                A: "Cheshire",
+                B: "Manchester",
+                C: "Stoke",
+                D: "Chester",
+            },
+            correctAnswer: "A"
         },
-        correctAnswer: "A"
-      },
-      {
-        question: "How old am I?",
-        answers: {
-          A: "23",
-          B: "25",
-          C: "27",
-          D: "29",
+        {
+            question: "How old am I?",
+            answers: {
+                A: "23",
+                B: "25",
+                C: "27",
+                D: "29",
+            },
+            correctAnswer: "C"
         },
-        correctAnswer: "C"
-      },
-      {
-        question: "What month is my birthday in?",
-        answers: {
-          A: "August",
-          B: "March",
-          C: "February",
-          D: "July",
+        {
+            question: "What month is my birthday in?",
+            answers: {
+                A: "August",
+                B: "March",
+                C: "February",
+                D: "July",
+            },
+            correctAnswer: "D"
         },
-        correctAnswer: "D"
-      },
-      {
-        question: "Who is my favourite sportsperson?",
-        answers: {
-          A: "Wayne Rooney",
-          B: "Mo Farah",
-          C: "Andy Murray",
-          D: "Shaun White",
+        {
+            question: "Who is my favourite sportsperson?",
+            answers: {
+                A: "Wayne Rooney",
+                B: "Mo Farah",
+                C: "Andy Murray",
+                D: "Shaun White",
+            },
+            correctAnswer: "D"
         },
-        correctAnswer: "D"
-      },
-      {
-        question: "Which is my favourite sport?",
-        answers: {
-          A: "Snowboarding",
-          B: "Tennis",
-          C: "Skiing",
-          D: "Hockey",
+        {
+            question: "Which is my favourite sport?",
+            answers: {
+                A: "Snowboarding",
+                B: "Tennis",
+                C: "Skiing",
+                D: "Hockey",
+            },
+            correctAnswer: "A"
         },
-        correctAnswer: "A"
-      },
-      {
-        question: "What vehicle do I own?",
-        answers: {
-          A: "Car",
-          B: "Motorbike",
-          C: "Scooter",
-          D: "Truck",
+        {
+            question: "What vehicle do I own?",
+            answers: {
+                A: "Car",
+                B: "Motorbike",
+                C: "Scooter",
+                D: "Truck",
+            },
+            correctAnswer: "B"
         },
-        correctAnswer: "B"
-      },
-      {
-        question: "Which country do I want to visit?",
-        answers: {
-          A: "Australia",
-          B: "China",
-          C: "Japan",
-          D: "Canada",
+        {
+            question: "Which country do I want to visit?",
+            answers: {
+                A: "Australia",
+                B: "China",
+                C: "Japan",
+                D: "Canada",
+            },
+            correctAnswer: "C"
         },
-        correctAnswer: "C"
-      },
-      {
-        question: "What colour are my eyes?",
-        answers: {
-          A: "Green",
-          B: "Hazel",
-          C: "Brown",
-          D: "Blue",
+        {
+            question: "What colour are my eyes?",
+            answers: {
+                A: "Green",
+                B: "Hazel",
+                C: "Brown",
+                D: "Blue",
+            },
+            correctAnswer: "A"
         },
-        correctAnswer: "A"
-      },
-      {
-        question: "What is my favourite food?",
-        answers: {
-          A: "Spaghetti",
-          B: "Curry",
-          C: "Burritos",
-          D: "Pizza",
-        },
-        correctAnswer: "D"
-      }
+        {
+            question: "What is my favourite food?",
+            answers: {
+                A: "Spaghetti",
+                B: "Curry",
+                C: "Burritos",
+                D: "Pizza",
+            },
+            correctAnswer: "D"
+        }
     ];
-  
+
     function buildQuiz() {
-      // we'll need a place to store the HTML output
-      const output = [];
-  
-      // for each question...
-      myQuestions.forEach((currentQuestion, questionNumber) => {
-        // we'll want to store the list of answer choices
-        const answers = [];
-  
-        // and for each available answer...
-        for (letter in currentQuestion.answers) {
-          // ...add an HTML radio button
-          answers.push(
-            `<label>
+        const output = [];
+
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            const answers = [];
+
+            for (letter in currentQuestion.answers) {
+                answers.push(
+                    `<label>
                <input type="radio" name="question${questionNumber}" value="${letter}">
                 ${letter} :
                 ${currentQuestion.answers[letter]}
              </label>`
-          );
-        }
-  
-        // add this question and its answers to the output
-        output.push(
-          `<div class="slide">
+                );
+            }
+
+            output.push(
+                `<div class="slide">
              <div class="question"> ${currentQuestion.question} </div>
              <div class="answers"> ${answers.join("")} </div>
            </div>`
-        );
-      });
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join("");
+            );
+        });
+
+        quizContainer.innerHTML = output.join("");
     }
-  
+
     function showResults() {
-      // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll(".answers");
-  
-      // keep track of user's answers
-      let numCorrect = 0;
-  
-      // for each question...
-      myQuestions.forEach((currentQuestion, questionNumber) => {
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if (userAnswer === currentQuestion.correctAnswer) {
-          // add to the number of correct answers
-          numCorrect++;
-  
-          // color the answers green
-          answerContainers[questionNumber].style.color = "darkgreen";
-        } else {
-          // if answer is wrong or blank
-          // color the answers red
-          answerContainers[questionNumber].style.color = "red";
-        }
-      });
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+        const answerContainers = quizContainer.querySelectorAll(".answers");
+
+        let numCorrect = 0;
+
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            if (userAnswer === currentQuestion.correctAnswer) {
+                numCorrect++;
+
+                answerContainers[questionNumber].style.color = "darkgreen";
+            } else {
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
+
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
-  
+
     function showSlide(n) {
-      slides[currentSlide].classList.remove("active-slide");
-      slides[n].classList.add("active-slide");
-      currentSlide = n;
-      
-      if (currentSlide === 0) {
-        previousButton.style.display = "none";
-      } else {
-        previousButton.style.display = "inline-block";
-      }
-      
-      if (currentSlide === slides.length - 1) {
-        nextButton.style.display = "none";
-        submitButton.style.display = "inline-block";
-      } else {
-        nextButton.style.display = "inline-block";
-        submitButton.style.display = "none";
-      }
+        slides[currentSlide].classList.remove("active-slide");
+        slides[n].classList.add("active-slide");
+        currentSlide = n;
+
+        if (currentSlide === 0) {
+            previousButton.style.display = "none";
+        } else {
+            previousButton.style.display = "inline-block";
+        }
+
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = "none";
+            submitButton.style.display = "inline-block";
+        } else {
+            nextButton.style.display = "inline-block";
+            submitButton.style.display = "none";
+        }
     }
-  
+
     function showNextSlide() {
-      showSlide(currentSlide + 1);
+        showSlide(currentSlide + 1);
     }
-  
+
     function showPreviousSlide() {
-      showSlide(currentSlide - 1);
+        showSlide(currentSlide - 1);
     }
-  
+
     const quizContainer = document.getElementById("quiz");
     const resultsContainer = document.getElementById("results");
     const submitButton = document.getElementById("submit");
-  
-    // display quiz right away
+
     buildQuiz();
-  
+
     const homeButton = document.getElementById("home");
     const previousButton = document.getElementById("previous");
     const nextButton = document.getElementById("next");
     const slides = document.querySelectorAll(".slide");
     const beginQuiz = document.querySelectorAll(".beginquiz");
     let currentSlide = 0;
-  
+
     showSlide(0);
-  
-    // on submit, show results
+
     submitButton.addEventListener("click", showResults);
     previousButton.addEventListener("click", showPreviousSlide);
     nextButton.addEventListener("click", showNextSlide);
-  })();
+})();
